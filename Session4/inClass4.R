a6f34eadb33bb0df77d6ab5a986f843d28113519
Ben <ben@avametric.com>
2019-02-25 15:03:15 -0800

    finished HW 4 more or less

--- Session4/inClass4.R
+++ Session4/inClass4.R
@@ -1,5 +1,5 @@
 
 library(dplyr)
-library(nycflights13f)
+library(nycflights13)
 flights %>% filter(month == 2) %>%
   filter(day == 20)%>%filter(year == 2013)%>% summarise((is.na(dep_delay)|is.na(arr_delay))
